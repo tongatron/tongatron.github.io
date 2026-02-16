2b4904209be3616ddd9920fca073e043ae4c463c
Giovanni Bindi <giovanni.bindi@gmail.com>
2026-02-16 19:11:49 +0100

    v4 memory

--- memory-pwa/sw.js
+++ memory-pwa/sw.js
@@ -1,21 +1,18 @@
-const CACHE="memory-v3";
+const CACHE_NAME = "memory-static-v4";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./manifest.json"
+];
 
-self.addEventListener("install",e=>{
-  e.waitUntil(
-    caches.open(CACHE).then(c=>c.addAll(["./","./index.html"]))
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(
-      keys.map(k=>k!==CACHE&&caches.delete(k))
-    ))
-  );
-});
-
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request))
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
