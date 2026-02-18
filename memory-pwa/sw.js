4e5088de520826c77f2115a2837671ebf1fe00ea
Giovanni Bindi <giovanni.bindi@gmail.com>
2026-02-18 19:32:50 +0100

    versioning

--- memory-pwa/sw.js
+++ memory-pwa/sw.js
@@ -2,7 +2,7 @@
    VERSIONE APP
 ================================ */
 
-const APP_VERSION = "1.1.2";
+const APP_VERSION = "1.1.3";
 const CACHE_NAME = "app-cache-" + APP_VERSION;
 
 /* ===============================
