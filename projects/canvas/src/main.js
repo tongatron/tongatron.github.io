c44d5d85ee4971f019ea50998f5b196341b42264
Giovanni Bindi <giovanni.bindi@gmail.com>
2026-04-11 23:13:33 +0200

    ok

--- projects/canvas/src/main.js
+++
@@ -1,8 +0,0 @@
-import Three from "./core/Three";
-import "./style.css";
-
-document.addEventListener("DOMContentLoaded", () => {
-	const container = document.querySelector("#app");
-	const three = new Three(container);
-	three.run();
-});
